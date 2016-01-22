2a85cd70de449a22947ac62d70237e86207c718a
Steven Bell <thestevenbell@gmail.com>
2016-01-21 21:14:56 -0500

    Changed precompiler back to the 'on' setting

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-config.serve_static_assets = false
+config.serve_static_assets = true
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
